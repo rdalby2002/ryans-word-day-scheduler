d77eba31de0a7e33b2ca868c74c94b0d27554594
rdalby2002 <ryancdalby@gmail.com>
2022-11-14 13:33:38 -0600

    Added colors to time blocks that change based on time of day

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,33 @@
 var currentDate = moment().format('dddd, MMM Do YYYY');
+
 $("#currentDay").html(currentDate);
+
+$(document).ready(function () {
+
+    function trackTime() {
+        var currentTime = moment().hour();
+
+        $(".time-block").each(function () {
+            var schedTime = parseInt($(this).attr("id"));
+            
+            if (schedTime === currentTime) {
+                $(this).removeClass("past");
+                $(this).removeClass("future");
+                $(this).addClass("present");
+            }
+            else if (schedTime < currentTime) {
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+
+            }
+        })
+    }
+
+    trackTime();
+})
